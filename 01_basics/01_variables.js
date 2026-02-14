ab05e69b274c76caab91c98e147bde80715f0743
Manthan <166823273+manthan2125@users.noreply.github.com>
2026-02-14 12:24:01 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12457;
+let accountEmail = "abxd@gmail.com";
+var accountPassword = "1245796255"; 
+accountCity = "Meerut";
+
+// accountId = 45998;  // Not Allowed
+accountEmail= "gcbfdf@gmail.com"
+accountPassword = "454789"
+accountCity = "Delhi"
+let accountState;
+
+console.log(accountEmail);
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
