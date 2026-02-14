b8b1f8281ebd4b2d206bb2535599d7bfd2f28dc7
Manthan <166823273+manthan2125@users.noreply.github.com>
2026-02-14 18:25:01 +0000

    conversion of datatypes

---
+++ 01_basics/01_datatypeConversion.js
@@ -0,0 +1,20 @@
+let score = "33sdcv"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+// "33" => 33
+// "33" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+// 1 => true ; 0 => false
+// "" => false
+// "tcy" => true
+
+// "33" => 33
