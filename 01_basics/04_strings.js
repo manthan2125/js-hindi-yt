75d6a20492d49865e4fe4ebdeff447d9e7ed033f
Manthan <166823273+manthan2125@users.noreply.github.com>
2026-02-15 18:11:54 +0000

    strings

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,28 @@
+let name = "Manthan-rathore-hcs"
+const repoCount = 5
+
+// console.log(name + repoCount + "Value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameScore = new String("Manthan-Rathore")
+
+// console.log(gameScore[0])
+// console.log(gameScore.__proto__);
+
+// console.log(gameScore.length);
+// console.log(gameScore.charAt(3));
+// console.log(gameScore.indexOf('t'));
+
+let name1 = "    harsh-gujral       "
+console.log(name1);
+console.log(name1.trim());
+console.log(name1.trimStart());
+console.log(name1.trimEnd());
+
+const url = "httpss://manthanrathore.com/manthan%20rathore"
+console.log(url.replace("%20", "-"));
+console.log(url.includes('manthan'));   // true
+
+console.log(name.split("-"));
+
