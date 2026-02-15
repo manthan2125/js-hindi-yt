f9430b0360afee429abe22b6652d8b1735895dd2
Manthan <166823273+manthan2125@users.noreply.github.com>
2026-02-15 17:42:38 +0000

    Rnaming FIle

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use Strict";  // Treats all JS code as newer verison
+
+// alert(3+3)  // We are using nodejs, not browser
+
+let name = "Manthan"
+let age = 20
+ /*
+ Types of Datatypes:
+ number
+ bigInt
+ boolean
+ string 
+ null => standalone value
+ undefined
+ symbol => unique
+
+
+
+
+ objects
+ */
+
+ console.log(typeof null)   // object
+ console.log(typeof undefined)  // undefined
+ 
---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,54 @@
+let score = "33sdcv"
+
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+// 1 => true ; 0 => false
+// "" => false
+// "tcy" => true
+
+// "33" => 33
+
+
+
+// ***************************************** Operations ***********************************************
+
+// console.log(1+2)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// ********************************** Comparisons ********************************
+
+// console.log("2">1)
+// console.log("1">1)
+
+// console.log(null > 0)       // false
+// console.log(null == 0)      // false
+// console.log(null >= 0)      // true
+
+console.log("2" == 2)    // true
+console.log("2" === 2)   // false
+
+//**************************** Summary *******************************
+
+// Primitive -> 7 Types : String, Number, Boolean,null,undefined,Symbol,BigInt
+// It is a Dynamically typed language
+
+// Reference Type(Non Primitve) -> Arrays,Objects,Functions
+
+
+
+
